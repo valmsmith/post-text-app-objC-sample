b7e4c29f57fde0bd61dfeb8743dac04b3b0bb3a4
valmsmith <valmsmith@users.noreply.github.com>
2015-08-19 14:49:22 -0700

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PostMediaAppSampleObjC
+//
+
+#import <UIKit/UIKit.h>
+
+
+#import <Parse/Parse.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
